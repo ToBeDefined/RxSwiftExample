6011ac386e832cb1baf5e37e9340869fe0cf50b2
TBD <weinanshao@163.com>
2017-12-13 18:10:07 +0800

    更新

--- TestRxSwift/LoginViewController.swift
+++ TestRxSwift/LoginViewController.swift
@@ -10,6 +10,42 @@ import UIKit
 import RxCocoa
 import RxSwift
 
+struct User {
+    let id: String
+    let nickname: String
+    
+    // 解析 json
+    init?(json: Any) {
+        guard
+            let dictionary = json as? [String: Any],
+            let id = dictionary["id"] as? String,
+            let nickname = dictionary["nickname"] as? String
+            else {
+                return nil
+        }
+        
+        self.id = id
+        self.nickname = nickname
+    }
+}
+
+
+enum GithubApi {
+    public static func login(username: String, password: String) -> Observable<User?> {
+        guard let baseURL = URL(string: "https://api.github.com"),
+            let url = URL(string: "login?username=\(username)&password=\(password)", relativeTo: baseURL) else {
+                return Observable.just(nil)
+        }
+        
+        return URLSession.shared.rx.json(url: url)
+            .catchError({ (e) -> Observable<Any> in
+                print(e)
+                return Observable.just(["id": "10000", "nickname": "luojie"]) // 由于此接口不存在，所以出错就直接返回演示数据
+            })
+            .map(User.init) // 解析 json
+    }
+}
+
 class LoginViewController: UIViewController {
     private let disposeBag = DisposeBag()
     
@@ -19,15 +55,19 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var pwdT: UILabel!
     @IBOutlet weak var loginBtn: UIButton!
     
+    @IBOutlet weak var imageV: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupRx()
+        let image = Observable.just(#imageLiteral(resourceName: "NoSelectRoundBtn"))
+        imageV.contentMode = .center
+        image.subscribeOn(MainScheduler.instance).bind(to: imageV.rx.image).disposed(by: disposeBag)
     }
     
     func setupRx() {
         guard let nameF = nameF, let pwdF = pwdF, let nameT = nameT,
             let pwdT = pwdT, let loginBtn = loginBtn else { return }
-        
         let nameIsValid = nameF.rx.text.orEmpty.map({ $0.count > 5 })
         let pwdIsValid  =  pwdF.rx.text.orEmpty.map({ $0.count > 5 })
         nameIsValid.bind(to: nameT.rx.isHidden).disposed(by: disposeBag)
@@ -40,7 +80,17 @@ class LoginViewController: UIViewController {
         let nameAndPwd = Observable.combineLatest(nameF.rx.text.orEmpty, pwdF.rx.text.orEmpty) { (nt, pt) -> (String, String)  in
             return (nt, pt)
         }
-        let rxUser = loginBtn.rx.tap.withLatestFrom(nameAndPwd)
+        let rxUser = loginBtn.rx.tap.withLatestFrom(nameAndPwd).do(onNext: { [weak self] (name, pwd) in
+            self?.login(name: name, pwd: pwd)
+        }).flatMapLatest(GithubApi.login)
+        
+        rxUser.observeOn(MainScheduler.instance).map ({ (user) -> String in
+            return user == nil ? "登录失败，请稍后重试" : "\(user!.nickname) 您已成功登录"
+        }).bind(to: loginBtn.rx.title()).disposed(by: disposeBag)
+    }
+    
+    func login(name: String, pwd: String) {
+        print("name => \(name), pwd => \(pwd)")
     }
 
 }
--- TestRxSwift/ViewController.swift
+++ TestRxSwift/ViewController.swift
@@ -19,16 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField2: UITextField!
     
     var subscription:Disposable?
-    
-    
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 // MARK: 3
-        class TestClass: ReactiveCompatible {
-            var name: String?
+        class TestClass: NSObject {
+            @objc var name: String?
         }
-        
         func creatMyObservable(_ obj: AnyObject) -> Observable<String?> {
             return Observable.create({ (obs) -> Disposable in
                 obs.onNext(obj.description)
@@ -62,26 +60,86 @@ class ViewController: UIViewController {
 //        }
         
 // MARK: 2
-        if let t1 = self.textField, let t2 = self.textField2 {
-            let o1 = t1.rx.text.map({ ($0 ?? "") })
-            let o2 = t2.rx.text.map({ ($0 ?? "") }).filter({ $0.count >= 3})
-            self.subscription = Observable.combineLatest(o1, o2, resultSelector: { (os1, os2) -> (String, Int, Int) in
-                return ("F: " + os1 + "  S: " + os2, os1.count, os2.count)
-            }).subscribe({ e in
-                switch e {
-                case .next(let el):
-                    print(el)
-                default:
-                    break
-                }
-            })
-        }
+//        if let t1 = self.textField, let t2 = self.textField2 {
+//            let o1 = t1.rx.text.map({ ($0 ?? "") })
+//            let o2 = t2.rx.text.map({ ($0 ?? "") }).filter({ $0.count >= 3})
+//            self.subscription = Observable.combineLatest(o1, o2, resultSelector: { (os1, os2) -> (String, Int, Int) in
+//                return ("F: " + os1 + "  S: " + os2, os1.count, os2.count)
+//            }).subscribe({ e in
+//                switch e {
+//                case .next(let el):
+//                    print(el)
+//                default:
+//                    break
+//                }
+//            })
+//        }
+        self.test()
     }
     
     
     
     @IBAction func cancelBtnClicked(_ sender: UIButton) {
         self.subscription?.dispose()
+    }
+}
+
+
+extension ViewController {
+    func test() {
+//        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        
+        typealias JSON = Any
+        
+        struct MyError: Error {
+            var errorCode: Int?
+            var errorString: String?
+            var errorData: Any?
+        }
+        
+        let json = Observable<JSON>.create { (observer) -> Disposable in
+            guard let url = URL.init(string: "http://www.baidu.com/") else {
+                observer.onError(MyError.init(errorCode: 10, errorString: "url error", errorData: nil))
+                return Disposables.create()
+            }
+            let request = URLRequest.init(url: url)
+            let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if error != nil {
+                    observer.onError(error!)
+                    return
+                }
+                
+                guard let jsonData = data, let jsonObj = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableLeaves) else {
+                    observer.onError(MyError.init(errorCode: 11, errorString: "json error", errorData: data))
+                    return
+                }
+                observer.onNext(jsonObj)
+                observer.onCompleted()
+            })
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+        
+        json.subscribe(onNext: { (json) in
+            print("Json success")
+        }, onError: { (error) in
+            if let error = error as? MyError {
+                print(error.errorCode)
+                print(error.errorString)
+                print(error.errorData)
+            } else {
+                print(error.localizedDescription)
+            }
+        }, onCompleted: {
+            print("completed")
+        }).disposed(by: disposeBag)
+        
+        
+        
+        
+//        >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     }
 }
 
