cfe9db39ce083a06248b996adcf4a5f5591a9e13
TBD <weinanshao@163.com>
2017-12-15 17:29:51 +0800

    添加ErrorHandling

---
+++ TestRxSwift/ErrorHandlingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ErrorHandlingViewController.swift
+//  TestRxSwift
+//
+//  Created by 邵伟男 on 2017/12/15.
+//  Copyright © 2017年 邵伟男. All rights reserved.
+//
+
+import UIKit
+
+class ErrorHandlingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
